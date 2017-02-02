c5ea9962aa208541d59e459179d03c42500e733e
Renan Martins <martins@fujisan.co.jp>
2017-02-01 16:48:49 -0800

    Add snipper for action creator with Axios

--- ActionCreator.js
+++ ActionCreator.js
@@ -5,10 +5,35 @@ export const acName = () => {
   };
 };
 
-// Async using Redux Thunk
 
+// Async using Redux Thunk
 export const acName = () => {
   return dispatch => {
 
+  };
+};
+
+
+// Using Axios
+export const acName = () => {
+  return dispatch => {
+  	dispatch({
+      type: VERB_RESOURCE_LOADING
+    });
+
+    return axios.get(`${ROOT_URL}/resource`)
+      .then(response => {
+        console.log(response.data);
+        return dispatch({
+          type: VERB_RESOURCE_SUCCESS,
+          payload: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        return dispatch({
+          type: VERB_RESOURCE_FAILURE
+        })
+      });
   };
 };
