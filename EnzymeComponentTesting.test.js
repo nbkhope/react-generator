f05f769e8d796785947e7fe4d48c0277c39b1ce3
Renan Martins <martins@fujisan.co.jp>
2017-02-10 13:36:22 -0800

    Add component testing file using Enzyme

---
+++ EnzymeComponentTesting.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('SomeComponent component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(
+      <SomeComponent />
+    );
+  });
+
+  test('should do something', () => {
+
+  });
+});
