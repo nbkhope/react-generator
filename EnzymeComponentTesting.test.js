154c36cd71f9ab138d85c4242d973fdae49d2d2c
Renan Martins <martins@fujisan.co.jp>
2017-02-10 13:47:51 -0800

    Add missing import statement

--- EnzymeComponentTesting.test.js
+++ EnzymeComponentTesting.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import SomeComponent from '../../components/SomeComponent';
+
 describe('SomeComponent component', () => {
   let component;
 
